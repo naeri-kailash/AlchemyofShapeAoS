e22dfcabbb2cbf027fb2da0ff7c518f58bcb4cdd
naeri-kailash <naerikailash@gmail.com>
2018-01-18 15:49:51 +1300

    set up landing page

---
+++ aos/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import logo from './AoSbeakerGirls.png'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to AoS</h1>
+          <h5>Naeri was here ;) ;)</h5>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App
